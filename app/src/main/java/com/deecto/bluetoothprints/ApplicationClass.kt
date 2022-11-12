2143a5f578c6e9169e7f7f4a3e6240bf53c28347
Sushant <sushantp053@gmail.com>
2022-11-12 18:26:43 +0530

    Initial commit

---
+++ app/src/main/java/com/deecto/bluetoothprints/ApplicationClass.kt
@@ -0,0 +1,12 @@
+package com.deecto.bluetoothprints
+
+import android.app.Application
+//import com.mazenrashed.printooth.Printooth
+
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        Printooth.init(context);
+    }
+}
